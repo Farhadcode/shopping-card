aa384927de1e98ea6a8e5a2af6771498c8e06c7f
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-08-18 02:23:50 +0600

    product price calculate

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('I am in a separate file')
+function getProductNumber(pdId,number, price ,isIncreasing  ) {
+    const productInput = document.getElementById(pdId);
+    let productNumber = productInput.value ;
+    if (isIncreasing == true) {
+        productNumber = parseFloat(productNumber) + 1;
+    }
+    else if(productNumber > 0){
+        productNumber = parseFloat(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+    const perProductTotal = document.getElementById(number);
+    perProductTotal.innerText = productNumber * price;
+}
+
+document.getElementById('phone-plus-btn').addEventListener('click',addProductFuntion =() =>{
+  getProductNumber('phone-number','phone',1219, true);
+  
+})
+
+document.getElementById('phone-minus-btn').addEventListener('click',minusProductFuntion = () =>{
+    getProductNumber('phone-number','phone', 1219, false);
+})
+
+
+// case
+
+document.getElementById('case-plus-btn').addEventListener('click',addProductFuntion =() =>{
+    getProductNumber('case-number','case' , 59 ,true);
+  
+})
+
+document.getElementById('case-minus-btn').addEventListener('click',minusProductFuntion = () =>{
+    getProductNumber('case-number', 'case' , 59 , false);
+})
